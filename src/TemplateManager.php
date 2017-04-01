439d3163b384f872b5247769733397787edeab03
rbonneil <late2201@gmail.com>
2017-04-01 13:49:41 +0200

    Implemantation d'un pointeur sur methode
    Le but de ce pointeur sur methode est de reunir les tags a chercher et les methodes a appeler lorsqu'on les trouve, ou si c'est precise, lorsqu'on ne les trouve pas

--- src/TemplateManager.php
+++ src/TemplateManager.php
@@ -7,6 +7,14 @@ class TemplateManager
     private $destination;
     private $destinationOfQuote;
 
+    private $replacements = [
+                             ['needle' => '[quote:destination_name]', 'method' => 'setDestinationName'],
+                             ['needle' => '[quote:summary_html]', 'method' => 'setSummaryHtml'],
+                             ['needle' => '[quote:summary]', 'method' => 'setSummary'],
+                             ['needle' => '[user:first_name]', 'method' => 'setFirstName'],
+                             ['needle' => '[quote:destination_link]', 'method' => 'setDestinationLink', 'ifNotFound' => 'setDestinationLinkDefault'] 
+                            ];
+
     public function getTemplateComputed(Template $tpl, array $data)
     {
         if (!$tpl) {
@@ -17,7 +25,6 @@ class TemplateManager
         $this->init($data);
         $replaced->subject = $this->computeText($replaced->subject, $data);
         $replaced->content = $this->computeText($replaced->content, $data);
-
 
         return $replaced;
     }
@@ -35,10 +42,7 @@ class TemplateManager
             $this->_quoteFromRepository = QuoteRepository::getInstance()->getById($quote->id);
             $this->usefulObject = SiteRepository::getInstance()->getById($quote->siteId);
             $this->destinationOfQuote = DestinationRepository::getInstance()->getById($quote->destinationId);
-
-            if(strpos($text, '[quote:destination_link]') !== false){
-                $this->destination = DestinationRepository::getInstance()->getById($quote->destinationId);
-            }
+            $this->destination = DestinationRepository::getInstance()->getById($quote->destinationId);
         }
     }
 
@@ -47,33 +51,49 @@ class TemplateManager
     **** Si elle en trouve un, elle le remplace et retourne le texte 
     ***/
     private function computeText($text, array $data)
-    { 
-        $containsSummaryHtml = strpos($text, '[quote:summary_html]');
-        $containsSummary     = strpos($text, '[quote:summary]');
-        $containsDestinationName = strpos($text, '[quote:destination_name]');
-        
-        if ($containsSummaryHtml !== false) {
-            $text = str_replace('[quote:summary_html]', Quote::renderHtml($this->_quoteFromRepository), $text);
+    {
+        // On boucle sur les tags qu'on recherche
+        for ($i = 0; $i < count($this->replacements); $i++) {
+            // On check si le tag est dans le texte
+            if (strpos($text, $this->replacements[$i]['needle'])) {
+                //Si oui, on appelle la methode de remplacement
+                $this->{$this->replacements[$i]['method']}($text, $this->replacements[$i]['needle']);
+            } else if (array_key_exists('ifNotFound', $this->replacements[$i])) {
+                //Si non, on appelle la methode par defaut, si elle est definie
+                $this->{$this->replacements[$i]['ifNotFound']}($text);
+            }
         }
-        if ($containsSummary !== false) {
-            $text = str_replace('[quote:summary]', Quote::renderText($this->_quoteFromRepository), $text);
-        }
-        if ($containsDestinationName !== false) {
-            $text = str_replace('[quote:destination_name]',$this->destinationOfQuote->countryName,$text);
-        }
-
-        
-        if (isset($this->destination)) {
-            $text = str_replace('[quote:destination_link]', $this->usefulObject->url . '/' . $this->destination->countryName . '/quote/' . $this->_quoteFromRepository->id, $text);
-        } else {
-            $text = str_replace('[quote:destination_link]', '', $text);
-        }
-
-        if (strpos($text, '[user:first_name]') !== false) {
-            $_user = (isset($data['user'])  and ($data['user']  instanceof User))  ? $data['user']  : ApplicationContext::getInstance()->getCurrentUser();
-            $text = str_replace('[user:first_name]', ucfirst(mb_strtolower($_user->firstname)), $text);
-        }
-
         return $text;
+    }
+
+    private function setSummaryHtml(&$text, $needle)
+    {
+         $text = str_replace($needle, Quote::renderHtml($this->_quoteFromRepository), $text);
+    }
+
+    private function setSummary(&$text, $needle)
+    {
+         $text = str_replace($needle, Quote::renderText($this->_quoteFromRepository), $text);
+    }
+
+    private function setDestinationName(&$text, $needle)
+    {
+        $text = str_replace($needle, $this->destinationOfQuote->countryName,$text);
+    }
+
+    private function setFirstName(&$text, $needle)
+    {
+         $_user = (isset($data['user'])  and ($data['user']  instanceof User))  ? $data['user']  : ApplicationContext::getInstance()->getCurrentUser();
+         $text = str_replace($needle, ucfirst(mb_strtolower($_user->firstname)), $text);
+    }
+    
+    private function setDestinationLink(&$text, $needle)
+    {
+         $text = str_replace('[quote:destination_link]', $this->usefulObject->url . '/' . $this->destination->countryName . '/quote/' . $this->_quoteFromRepository->id, $text);
+    }
+
+    private function setDestinationLinkDefault(&$text)
+    {
+         $text = str_replace('[quote:destination_link]', '', $text);
     }
 }
